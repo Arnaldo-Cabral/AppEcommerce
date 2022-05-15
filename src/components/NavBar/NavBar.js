e5dc4b1d4e78b3cd1453d7e6f4e678227c3399ea
Arnaldo-Cabral <arnaldo.ela@gmail.com>
2022-05-15 12:10:38 -0300

    modificacion navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import './NavBar.css'
+const NavBar =() => {
+  return (
+    <nav>
+      <h1>Ecommerce</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Inicio</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Celulares</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Tablets</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Notebooks</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Logearse</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </nav>
+  )
+} 
+
+export default NavBar
