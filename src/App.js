724d867aaf1e98c87e8bbde437c8218e458dbcf9
Arnaldo-Cabral <arnaldo.ela@gmail.com>
2022-05-29 16:58:16 -0300

    consumiendo api mercado libre

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+//import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+//import ItemCount from './components/ItemCount/ItemCount';
+import MercadoLibre from './components/MercadoLibre/MercadoLibre';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting='Hola Coders' />
+      {/* <ItemListContainer greeting='Hola Coders' /> */}
+      <MercadoLibre />
       
     </div>
   );
