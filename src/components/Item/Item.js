49fbe97d373b19fef740c3930f69e28f058382e1
Arnaldo-Cabral <arnaldo.ela@gmail.com>
2022-06-20 00:06:03 -0300

    se elimino de la cart el id

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,7 +7,6 @@ const Item = ({id, name, price, img}) => {
 
     return (
         <div className="card">
-            <p className="idNumero">{id}</p>
             <img className="imgCarrito" src={img} />
             <div className="container">
                 <h4 className="nombreProducto"><b>{name}</b></h4>
